e831fc9379cce15ab5cbf4ff728ff03ab7c39010
sayedfarhad <sayedfarhad@sayeds-imac.hsd1.ca.comcast.net>
2023-01-21 11:48:35 -0800

    I added

--- src/test/java/com/noorteck/qa/runners/Runner.java
+++ src/test/java/com/noorteck/qa/runners/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "src/test/resources/hr/US205-GetAllDeps.feature", glue = "com.noorteck.qa.steps", plugin = {
-		"summary", "pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports",
+		"summary", "pretty", "html:target/cucumber-reports.html", "json:target/destination/cucumber-reports.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
 public class Runner {
 
